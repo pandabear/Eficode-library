2712fb22b9a17820be3de9c91658bcefca3a8bbb
Panda Bear <your.email@here.com>
2012-01-20 16:55:32 +0200

    Initial rails 3 application with the guidance under Tair Assimov.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Library::Application.config.secret_token = '5d376bf8ada72a6f173e9cf4e2659603e21de6eb8f876e17371e6927e8534fb2b6310e3761bba1e9a0f960be3ff85d2f5de1c539c168b2cf511f1bfdba96d557'
